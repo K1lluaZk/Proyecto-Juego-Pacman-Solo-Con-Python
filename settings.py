abdbc63798c5a03d85aa11846dbb060f3ad32478
Mario Suero <mario.suerodejesus@gmail.com>
2026-08-15 23:56:08 -0400

    feat: add settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+# settings.py
+# Constantes de configuración del juego (dimensiones, colores, fps, etc.)
+
+WIDTH = 900
+HEIGHT = 950
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Color de las paredes del laberinto (usado en draw_board)
+WALL_COLOR = "blue"
